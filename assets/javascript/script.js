0bbd1fb71ef10c7905d4f78d10bdf9c73145da98
dgallardo06 <denise.gallardo1@gmail.com>
2018-01-14 19:10:03 -0500

    function to add new buttons to array and display gifs for each sport in array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,54 +1,12 @@
 $( document ).ready(function() {
 
-  //API Key: 5KfAbfQbUKgHyuM06tjXgrr3Gj64orZV
   //Array of movies
   var topics = ["basketball", "soccer", "skateboarding", "baseball", "volleyball"];
 
   renderButtons();
+  showGifs();
 
-
-
-  $("button").on("click", function(){ 
-    //var sport = "soccer";
-    //var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&limit=10&api_key=5KfAbfQbUKgHyuM06tjXgrr3Gj64orZV";
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=basketball&limit=10&api_key=5KfAbfQbUKgHyuM06tjXgrr3Gj64orZV";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).done(function(response){
-      var results = response.data;
-        console.log("ready");
-
-      for (var i=0; i < results.length; i++){
-
-        //Create div to hold movie
-        var gifDiv = $("<div class='gif'>");
-
-        //Store rating
-        var rating = results[i].rating;
-        console.log(rating);
-
-        //P element to display rating
-        var ratingP = $("<p>").text("Rating: " + rating);
-
-        //Get URL of gif
-        var gifURL = results[i].images.fixed_height_still.url;
-        console.log(gifURL);
-
-        //Create image element to hold gif
-        var sportGif = $("<img>").attr("src", gifURL);
-
-        //Append the gif
-        gifDiv.append(sportGif);
-        gifDiv.prepend(ratingP);
-
-        //
-        $("#gifsView").append(gifDiv);
-      }
-    })
-
-/*  $("#add-sport").on("click", function(event){
+  $("#add-sport").on("click", function(event){
     event.preventDefault();
 
     var sport = $("#sport-input").val().trim();
@@ -56,10 +14,8 @@ $( document ).ready(function() {
     topics.push(sport);
 
     renderButtons();
-
-  });*/
-
-});
+    showGifs();
+  });
 
 
   //===============Functions=================
@@ -78,6 +34,44 @@ $( document ).ready(function() {
           $("#buttonsNav").append(newButton);
         }
       }
-      
+
+
+      function showGifs(){
+        $("button").on("click", function(){ 
+        var sport = $(this).attr("data-name");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + sport + "&limit=10&api_key=5KfAbfQbUKgHyuM06tjXgrr3Gj64orZV";
+
+        $.ajax({
+          url: queryURL,
+          method: "GET"
+        }).done(function(response){
+        var results = response.data;
+
+        for (var i=0; i < results.length; i++){
+
+          //Create div to hold movie
+          var gifDiv = $("<div class='gif'>");
+
+          //Store rating
+          var rating = results[i].rating;
+
+          //P element to display rating
+          var ratingP = $("<p>").text("Rating: " + rating);
+
+          //Get URL of gif
+          var gifURL = results[i].images.fixed_height_still.url;
+
+          //Create image element to hold gif
+          var sportGif = $("<img>").attr("src", gifURL);
+
+          //Append the gif
+          gifDiv.append(sportGif);
+          gifDiv.prepend(ratingP);
+
+          $("#gifsView").prepend(gifDiv);
+        }
+      })
+    });
+  }   
       
 });
